19c6bf6fc76a323f3d17eac0b910107558d2f024
Redwanul Abedin <chromeplated@protonmail.com>
2021-11-27 14:34:48 -0500

    Initial commit

---
+++ src/structs.rs
@@ -0,0 +1,30 @@
+// Structs - Used to create custom data types
+
+// Traditional Struct
+//    struct Color {
+//        red: u8,
+//        green: u8,
+//        blue: u8
+//    }
+
+// Tuple Struct
+//struct Color(u8, u8, u8);
+
+struct Person {
+    first_name: String
+}
+
+//pub fn run() {
+//    let mut c = Color {
+//        red: 255,
+//        green: 0,
+//        blue: 0
+//    };
+
+//    c.red = 200;
+
+//    println!("Color: {} {} {}", c.red, c.green, c.blue);
+      
+//    let mut c = Color(255, 0, 0);
+//    println!("Color: {} {} {}", c.0, c.1, c.2);
+//}
